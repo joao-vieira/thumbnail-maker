c2d5f7be7bc377dffa4c883c72b6044d0a6b9e66
Joao Vieira <joaoportugalv@gmail.com>
2022-10-02 22:16:37 +0900

    Long running job architecture implemented, readme added

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# thumbnail-maker
- 
+### Thumbnail Maker
+
+## Description
+This API receives an image in Base64 format, and resizes it to 100x100px. Since image processing can take time, a long-running job architecture was implemented. For the purpose of this exercise, a simple implementation with the library flask_executor was implemented.
+
+## How to Use
+To start the server, run `docker-compose up`
+
+## Requests
+The server receives POST requests in the form of "/image" and resizes the image. Once the image is ready, it can be requested with a GET request at "/result"
+
+Example image request:
+`curl -X POST -d "image=iVBORw0KGgoAAAANSUhEUgAAABIAAAAPCAIAAABm5AhFAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAAFiUAABYlAUlSJPAAAAA6SURBVDhPY/hPFhiS2hgYCIsAwcA6kkhADW1YfY8M4ArQteHSiSaFrggijRVAVYABdrMJAnpq%2B/8fAFBw9CiMWfoDAAAAAElFTkSuQmCC" "http://localhost:5000/image"`
+
+Example result request:
+`curl "http://localhost:5000/result"`
--- server.py
+++ server.py
@@ -1,20 +1,33 @@
-from flask import Flask,request
+from flask import Flask, request, jsonify
 from thumbnail_maker import image_converter
-import sys
+from flask_executor import Executor, executor
+
 
 app = Flask(__name__)
+executor = Executor(app)
 
 @app.route('/image', methods=['POST'])
 def resize_image():
     try:
         original_file = request.form.get('image')
-        resized_image_data = image_converter(original_file)
+        executor.submit_stored('image_converter', image_converter, original_file)
 
-        return resized_image_data
+        return jsonify({'result':'success'})
 
     except Exception as err:
-        pass
+        print(err)
 
+@app.route('/result', methods=['GET'])
+def get_image():
+    try:
+        if not executor.futures.done('image_converter'):
+            future_status = executor.futures._state('image_converter')
+            return jsonify({'status': future_status})
+        future = executor.futures.pop('image_converter')
+        return future.result()
+
+    except Exception as err:
+        print(err)
 
 if __name__ == '__main__':
     app.run(host="localhost", port=5000)
